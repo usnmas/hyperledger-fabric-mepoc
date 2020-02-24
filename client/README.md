c87d139abd5ca791d9a8c596811bc0785f707019
Yup <43056998+usnmas@users.noreply.github.com>
2020-02-24 14:32:14 +1300

    Add files via upload

---
+++ client/README.md
@@ -0,0 +1,7 @@
+# Usage 
+
+
+- Before execution of any node file, package should be installed in this folder. 
+
+- Once "enrollAdmin" has been run, "wallet" subfolder will be created with public/private keys. 
+
---
+++ client/query.js
@@ -0,0 +1,49 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { FileSystemWallet, Gateway } = require('fabric-network');
+const path = require('path');
+
+const ccpPath = path.resolve(__dirname, '..', 'basic-network', 'connection.json');
+
+async function main() {
+    try {
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists('admin');
+        if (!userExists) {
+            console.log('An identity for "admin" does not exist in the wallet');
+            console.log('Run the enrollAdmin.js application before retrying');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccpPath, { wallet, identity: 'admin', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('mycc');
+
+        // Evaluate the specified transaction.
+        // TEST /////////////////////////////////////
+        const result = await contract.evaluateTransaction('queryData','s001');
+        console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        process.exit(1);
+    }
+}
+
+main();
