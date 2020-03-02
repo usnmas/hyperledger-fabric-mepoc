bf21d9b21ebfeff02bc9c3ef1d13947a9de027ef
Yup <43056998+usnmas@users.noreply.github.com>
2020-03-02 16:40:41 +1300

    Create query.sh

---
+++ client/query.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONFIG_ROOT=/opt/gopath/src/github.com/hyperledger/fabric/peer
+source src.conf
+
+echo "Query State DB record by Device ID"
+docker exec \
+  -e CORE_PEER_LOCALMSPID=Org1MSP \
+  -e CORE_PEER_MSPCONFIGPATH=${CONFIG_ROOT}/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp \
+  cli \
+  peer chaincode query \
+    -n $cc \
+    -c '{"Args":["'$func'","'$id'"]}' \
+    -C mychannel
