2da4189c6850372e7b2b53f846b9eedaca847ee0
Yup <43056998+usnmas@users.noreply.github.com>
2020-03-02 18:02:47 +1300

    Update os_test_data_create.js

--- scripts/os_test_data_create.js
+++ scripts/os_test_data_create.js
@@ -1,8 +1,9 @@
 /*
- * This Node JS file is for test data creation for PoC 
- * This needs an argument for temperature data
- * OUTPUT : "SensorID","ActuatorID","TimeStamp (Millisecond)","Temperature","Hash Value"
- */
+ *  This Node JS file is for test data creation for PoC 
+ *  This needs an argument for temperature data
+ *  OUTPUT : "SensorID","ActuatorID","TimeStamp (Millisecond)","Temperature","Hash Value"
+ *    
+*/
 
 // timestamp
 var cTime = new Date();     // 2020-02-17T09:53:33.658Z
@@ -11,16 +12,23 @@ var ts = cTime.getTime();   // 1581933213658 (Millisecond)
 // Checksum value creation
 var sha256 = require("js-sha256");
 
-// Adapt temperature as an input parameter for actuator enabling & disabling
-process.argv.forEach((val, index) => {});
+// Adapt temperature as input parameter
+process.argv.forEach((val, index) => {}); 
 
 var sid = "s001";
 var aid = "a001";
 //var tem = "35";
-var tem = process.argv[2];
+var tem = process.argv[2]; 
 var dat = sid + aid + ts + tem;
 var has = sha256(dat);
 var tsS = String(ts);
 var msgArr = [sid, aid, tsS, tem, has];
 
 console.log(JSON.stringify(msgArr));
+
+console.log("a1="+sid); 
+console.log("a2="+aid); 
+console.log("a3="+tsS); 
+console.log("a4="+tem); 
+console.log("a5="+has); 
+
