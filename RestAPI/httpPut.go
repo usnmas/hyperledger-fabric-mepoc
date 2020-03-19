84e992aa58ce70c603dadf0d521e6ca8160bd55a
Yup <43056998+usnmas@users.noreply.github.com>
2020-03-19 16:16:27 +1300

    Create httpPut.go

---
+++ RestAPI/httpPut.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+"fmt"
+"strings"
+"net/http"
+"io/ioutil"
+)
+
+func main() {
+
+  url := "130.195.10.173:8080/api/changeowner/CAR4"
+  method := "PUT"
+  payload := strings.NewReader("{\"owner\":\"XYZ\"}")
+
+  client := &http.Client {
+  }
+  
+  req, err := http.NewRequest(method, url, payload)
+  if err != nil {
+    fmt.Println(err)
+  }
+  req.Header.Add("Content-Type", "application/json")
+
+  res, err := client.Do(req)
+  defer res.Body.Close()
+  body, err := ioutil.ReadAll(res.Body)
+
+  fmt.Println(string(body))
+}
